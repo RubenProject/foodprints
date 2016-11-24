fe15821735f83d024ed205d6bda3e548ccb9e46d
rubenproject <r.vdwaal1995@gmail.com>
2016-11-24 18:12:23 +0100

    completed questionair backend

--- graphview/urls.py
+++ graphview/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^random_recipe/', views.random_recipe, name='random_recipe'),
+    url(r'^view/(?P<recipe>[A-Z a-z]+)$', views.view, name='view'),
     url(r'^question/(?P<question_num>[0-9]+)/$', views.question, name='question'),
     url(r'^answer/(?P<question_num>[0-9]+)/$', views.answer, name='answer'),
 ]
--- graphview/views.py
+++ graphview/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, get_list_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from graphview.models import Recipe, Ingredient
+from django.core.urlresolvers import reverse
+import random
 
 
-_color = ''
-_country_of_origin = ''
-_ingredient_1 = ''
-_ingredient_2 = ''
+def random_recipe(request):
+    recipe_list = get_list_or_404(Recipe.objects.all())
+    recipe = ''.join(random.choice(recipe_list).recipe_name).encode("ascii")
+    return HttpResponseRedirect(reverse('graphview:view', args=(recipe,)))
 
-
-
-def index(request):
-    return render(request, 'graphview/index.html')
+def view(request, recipe):
+    return render(request, 'graphview/view.html')
 
 def question(request, question_num):
     if int(question_num) == 1:
@@ -22,25 +22,56 @@ def question(request, question_num):
         return render(request, 'graphview/question.html', {
             'question': "What color is your food?",
             'question_num': question_num,
-            'recipe_list': color_str_list,
+            'option_list': color_str_list,
             })
     if int(question_num) == 2:
-        return HttpResponse(question_num)
+        recipe_list = Recipe.objects.filter(color=request.session['color'])
+        country_list = get_list_or_404(recipe_list.order_by('country_of_origin').values_list('country_of_origin').distinct())
+        country_str_list = []
+        for country in country_list:
+            country_str_list.append(''.join(country[0]).encode("ascii"))
+        return render(request, 'graphview/question.html', {
+            'question': "What country is your food from?",
+            'question_num': question_num,
+            'option_list': country_str_list,
+            })
     if int(question_num) == 3:
-        return HttpResponse(question_num)
-
+        recipe_list = Recipe.objects.filter(color=request.session['color'])
+        recipe_list = recipe_list.filter(country_of_origin=request.session['country_of_origin'])
+        ingredient_list = get_list_or_404(Ingredient.objects.filter(recipe=recipe_list).distinct())
+        return render(request, 'graphview/question.html', {
+            'question': "Which of these ingredients is in your food?",
+            'question_num': question_num,
+            'option_list': ingredient_list,
+            })
+    if int(question_num) == 4:
+        recipe_list = Recipe.objects.filter(color=request.session['color'])
+        recipe_list = recipe_list.filter(country_of_origin=request.session['country_of_origin'])
+        recipe_list = recipe_list.filter(ingredients=Ingredient.objects.filter(ingredient_name=request.session['ingredient1']))
+        ingredient_list = get_list_or_404(Ingredient.objects.filter(recipe=recipe_list).distinct())
+        return render(request, 'graphview/question.html', {
+            'question': "Which of these ingredients is in your food?",
+            'question_num': question_num,
+            'option_list': ingredient_list,
+            })
+    if int(question_num) == 5:
+        recipe_list = Recipe.objects.filter(color=request.session['color'])
+        recipe_list = recipe_list.filter(country_of_origin=request.session['country_of_origin'])
+        recipe_list = recipe_list.filter(ingredients=Ingredient.objects.filter(ingredient_name=request.session['ingredient1']))
+        recipe_list = recipe_list.filter(ingredients=Ingredient.objects.filter(ingredient_name=request.session['ingredient2']))
+        recipe = ''.join(random.choice(recipe_list).recipe_name).encode("ascii")
+        return HttpResponseRedirect(reverse('graphview:view', args=(recipe,)))
 
 def answer(request, question_num):
-    if int(question_num) == 1:
-        try:
-            _color = request.POST['answer']
-        except (KeyError, Recipe.DoesNotExist):
-            return(request, 'graphview/question.html', {
-                'question': "What color is your food?",
-                'question_num': question_num,
-                'recipe_list': color_str_list,
-                })
-    if int(question_num) == 2:
-        return HttpResponse(question_num)
-    if int(question_num) == 3:
-        return HttpResponse(question_num)
+    if (request.POST.get('answer', False) == False):
+        return HttpResponseRedirect(reverse('graphview:question', args=(int(question_num),)))
+    else:
+        if int(question_num) == 1:
+            request.session['color'] = request.POST.get('answer', False)
+        if int(question_num) == 2: 
+            request.session['country_of_origin'] = request.POST.get('answer', False)
+        if int(question_num) == 3:
+            request.session['ingredient1'] = request.POST.get('answer', False)
+        if int(question_num) == 4:
+            request.session['ingredient2'] = request.POST.get('answer', False)
+        return HttpResponseRedirect(reverse('graphview:question', args=(int(question_num)+1,)))
