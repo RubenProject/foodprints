2b304afb89cd400437069b812a84778d053433ae
rubenproject <r.vdwaal1995@gmail.com>
2016-11-25 15:47:21 +0100

    added CSS to all pages

--- foodprints/urls.py
+++ foodprints/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('home.urls')),
+    url(r'^', include('home.urls', namespace="home")),
     url(r'^graphview/', include('graphview.urls', namespace="graphview")),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
